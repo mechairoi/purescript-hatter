5c64927baf33ff32c3556fe566f2188068b35f3a
mechairoi <ttsujikawa@gmail.com>
2017-02-22 18:55:17 +0900

    rid unused files

--- bin/hatter.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-"use strict;"
-var hatter = require('output/node_modules/index.js');
-var input = "";
-process.stdin.on('data', function(chunk) { input += chunk; });
-process.stdin.on('end', function() { console.log(hatter([])(input).value0); });
