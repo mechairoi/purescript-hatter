91cc1e312db430c2d5162f7fcdc530209e8b3501
mechairoi <ttsujikawa@gmail.com>
2017-02-22 18:49:54 +0900

    Process all files in the directory.

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,35 @@
-/* global exports, process, console */
+/* global exports, process, console, require */
 "use strict";
+
+var glob = require("glob");
+var path = require("path");
+var fs = require("fs");
+
+function run(hatter) {
+  var target = process.argv[2];
+  var files = glob.sync(target + "/**/*.hat");
+  files.forEach(function (inpath) {
+    var p = path.parse(inpath);
+    var outpath = path.format({ dir: p.dir, base: p.name + ".purs" });
+    console.log("Generating `" + outpath + "` from `" + inpath  + "`");
+    var input = fs.readFileSync(inpath, "utf8");
+    var output = hatter(input);
+    fs.writeFileSync(outpath, output);
+  });
+}
 
 exports.mainImpl = function (hatter) {
   return function (isRight) {
     return function () {
-      process.stdin.resume();
-      var input = "";
-      process.stdin.on("data", function (chunk) {
-        input += chunk;
-      });
-      process.stdin.on("end", function () {
+      var h = function (input) {
         var result = hatter([])(input);
         if (isRight(result)) {
-          process.stdout.write(result.value0);
+          return result.value0;
         } else {
-          console.log(result.value0);
+          throw result.value0;
         }
-      });
+      };
+      run(h);
     };
   };
 };
